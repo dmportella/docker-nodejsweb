2dde3713a8232987743b5eedbcda2ae1e8b1f6bd
daniel portella <dmportell@hotmail.com>
2016-05-11 13:04:06 +0100

    Added clustering so it makes use of all cores

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-var http = require('http');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.PORT || 8080);
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+	var http = require('http');
+
+	// Configure our HTTP server to respond with Hello World to all requests.
+	var server = http.createServer(function (request, response) {
+	  response.writeHead(200, {"Content-Type": "text/plain"});
+	  response.end("Hello World\n");
+	});
+
+	// Listen on port 8000, IP defaults to 127.0.0.1
+	server.listen(process.env.PORT || 8080, function(){
+	    var addr = server.address();
+    	console.log('#', cluster.worker.id, "|Server listening at", addr.address + ":" + addr.port);
+  	});
+}
